36267101726d95b9cf93cb140ae850b8b3d3ad9b
xhc <2923637940@qq.com>
2023-10-26 22:32:08 +0800

    品茶页面1.0

--- src/views/TasteView.vue
+++ src/views/TasteView.vue
@@ -1,20 +1,89 @@
 <template>
     <div class="taste">
-      <div style="display: flex;margin-top: 124px;margin-left: 90px;">
+
+      <div style="display: flex;margin-top: 124px;margin-left: 54px;">
+
           <div>  
-          <t-divider style="width: 400px;left:292px; height:2px;opacity: 1;background: rgba(211, 217, 191, 1);"></t-divider> 
-          <div>
-          <div>
+          <t-divider style="width: 520px;height:2px;margin-right: 40px;background: rgba(211, 217, 191, 1);"></t-divider> 
+          <div style="display: flex;">
+          <div style="margin-right: 100px;">
           <p style="font-size: 18px;font-weight: 500;letter-spacing:0px;line-height:26.06px;color: rgba(50, 132, 110, 1);text-align: left;vertical-align">评鉴方法</p>
           <p style="font-size: 12px;font-weight: 500;letter-spacing: 0px;line-height: 17.38px;color: rgba(50, 132, 110, 1);text-align: left;vertical-align: middle;">METHOD</p>
-          <p style="font-size: 10px;font-weight: 500;letter-spacing: 0px;line-height: 11.72px;color: rgba(50, 132, 110, 0.5);text-align: left;vertical-align: middle; ">说明文字：这是说明文字这是说明文字这是说明文字这是说明文字这是说明文字这是说明文字这是说明文字这是说明文字这是说明文字这是说明文字这是说明文字。</p>
-          </div>   
+          </div>  
+          <p style="font-size: 10px;font-weight: 500;letter-spacing: 0px;line-height: 11.72px;color: rgba(50, 132, 110, 0.5);text-align: left;vertical-align: middle; ">说明文字：这是说明文字这是说明文字这是说明文字这是说明文字</p>
           </div>
+
+          <template>
+            <t-card style="width:520px;height:331px;border:0px;margin-bottom: 26px;margin-top: 19px;background: rgba(255, 249, 232, 1)"> 
+            
+            </t-card>
+
+            <t-card style="width:520px;height:331px;border:0px;margin-bottom: 26px;background: rgba(255, 249, 232, 1)"> 
+
+            </t-card>
+
+            <t-card style="width:520px;height:331px;border:0px;margin-bottom: 26px;background: rgba(255, 249, 232, 1)"> 
+
+            </t-card>
+
+            <t-card style="width:520px;height:331px;border:0px;margin-bottom: 26px;background: rgba(255, 249, 232, 1)"> 
+
+            </t-card>
+
+          </template>
+          
           </div>
-          <t-divider style="width: 400px;left:852px; height:2px;opacity: 1;margin-right: 50vh;background: rgba(211, 217, 191, 1);"></t-divider>
+
+          <div>
+          <t-divider style="width: 520px;height:2px;background: rgba(211, 217, 191, 1);"></t-divider>
+          <div style="margin-right: 100px;">
+          <p style="font-size: 18px;font-weight: 500;letter-spacing:0px;line-height:26.06px;color: rgba(50, 132, 110, 1);text-align: left;vertical-align">感受</p>
+          <p style="font-size: 12px;font-weight: 500;letter-spacing: 0px;line-height: 17.38px;color: rgba(50, 132, 110, 1);text-align: left;vertical-align: middle;">FEELING</p>
+          </div>  
+
+          <template >
+            <div style="display: flex;">
+            <t-card style="width:100px;height:100px;border:0px;margin-top: 12px;margin-right: 40px;background: rgba(245, 244, 222, 1)"> 
+            
+            </t-card>
+
+            <t-card style="width:100px;height:100px;border:0px;margin-top: 10px;margin-right: 40px;background: rgba(245, 244, 222, 1)"> 
+            
+            </t-card>
+
+            <t-card style="width:100px;height:100px;border:0px;margin-top: 10px;margin-right: 40px;background: rgba(245, 244, 222, 1)"> 
+            
+            </t-card>
+
+            <t-card style="width:100px;height:100px;border:0px;margin-top: 10px;margin-right: 68px;background: rgba(245, 244, 222, 1)"> 
+            
+            </t-card>
           </div>
-      <h1>品茶</h1>
-      <p>TASTE</p>
+
+          <div style="display: flex;">
+
+            <div>
+            <p style="left: 8px;
+top: 102px;
+width: 88px;
+height: 23px;
+opacity: 1;font-size: 12px;font-weight: 500;line-height: 14.06px;color: rgba(50, 132, 110, 1);text-align: center;vertical-align: middle;margin-top: 2px;">口感</p>
+            <p style="left: 0px;
+top: 118px;
+width: 100px;
+height: 34px;
+opacity: 1;font-size: 8px;font-weight: 400;line-height: 15px;color: rgba(50, 132, 110, 0.5);text-align: center;vertical-align: middle;">这是说明文字</p>
+          </div>
+
+          </div>
+
+          </template>  
+
+          </div>
+          
+      </div>
+
+
       
     </div>
   </template>
@@ -24,6 +93,6 @@
   
   export default {
     name: 'TasteView',
-  
   }
+  
   </script>
