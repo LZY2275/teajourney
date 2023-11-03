3e92ba7f6787b5bd6e2164882d1a18cbdda3302a
= <=>
2023-11-03 19:18:42 +0800

    完成销茶页面的框架

---
+++ src/views/TeaGardenChartView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="tea-garden">
+        <h1>茶园面积数据表</h1>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  
+  export default {
+    name: 'TeaGardenChartView',
+  
+  }
+  </script>
