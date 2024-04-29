5a2f9db17823cb77185e9f8720cd64d307f53683
= <=>
2024-04-29 13:37:36 +0800

    首页添加交互

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="global" >
-    <IntroOverlay></IntroOverlay>
+    <IntroOverlay style="position: absolute;z-index: 999;"></IntroOverlay>
     <div style="padding:5.17578vh 5.347vw 0 5.347vw;display: flex;align-items: center;">
       <div style="display: flex;">
         <div class="top">
@@ -37,6 +37,17 @@
       <p>{{ titleen }}</p>
     </div>
 
+    <!-- 右下方页面卡片实现 -->
+    <div style="width:70vw;height: 73vh;position: absolute;right: 0;bottom: -16px;overflow: hidden;">
+      <!-- 卡片1 -->
+      <div style="width: 69vw;height: 68vh;position: absolute;top: 3vw;left: 3vw;background-color: #0d6b5624;" class="card card1"></div>
+      <div style="width: 69vw;height: 68vh;position: absolute;top: 5vw;left: 10vw;background-color:#0d6b5650;" class="card card2"></div>
+      <div style="width: 69vw;height: 68vh;position: absolute;top: 7vw;left: 17vw;background-color:#0d6b5670;" class="card card3"></div>
+      <div style="width: 69vw;height: 68vh;position: absolute;top: 9vw;left: 24vw;background-color:#0d6b5690;" class="card card4"></div>
+      <div style="width: 69vw;height: 68vh;position: absolute;top: 11vw;left: 31vw;background-color:#0d6b5695;" class="card card5"></div>
+      <div style="width: 69vw;height: 68vh;position: absolute;top: 13vw;left: 38vw;background-color:#0d6b56;" class="card card6"></div>
+    </div>
+
   </div>
 </template>
 
@@ -69,7 +80,7 @@ export default {
     },
   },
   components:{
-    IntroOverlay
+    IntroOverlay,
   }
 }
 </script>
@@ -110,5 +121,58 @@ export default {
     background-size: cover;
     background-position: 0 20vh;
   }
+
+  .card:hover{
+    /* transform: scale(1.05);
+     */
+     /* 向左上方移动 */
+    transform: translate(-1vw,-1vw);
+    transition: all 0.2s;
+  }
+
+  .card{
+    transition: all 0.2s;
+  }
+
+  .card{
+    border-radius: 2vw;
+    border:0.8vw solid #D3D9BF;
+    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
+    background-repeat: no-repeat;
+    background-clip: padding-box;
+    background-size: cover;
+  }
+
+  .card1{
+    background-image: url("../assets/img/yemian1.png");
+    opacity: 0.5;
+  }
+
+  .card2{
+    background-image: url("../assets/img/yemian2.png");
+    opacity: 0.6;
+  }
+
+  .card3{
+    background-image: url("../assets/img/yemian3.png");
+    opacity: 0.7;
+  }
+
+  .card4{
+    background-image: url("../assets/img/yemian4.png");
+    opacity: 0.8;
+  }
+
+  .card5{
+    background-image: url("../assets/img/yemian5-1.png");
+    opacity: 0.9;
+  }
+
+  .card6{
+    background-image: url("../assets/img/yemian6.png");
+    opacity: 1;
+  }
+
+
 
 </style>
